b7c650963a080e2378ee07412b0546afb8917205
sachidumaleesha <diwansachidu2000@gmail.com>
2024-04-05 16:23:31 +0530

    initialize project
    setup tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='className="text-3xl font-bold underline'>
+      Diwan Sachidu
+    </div>
+  )
+}
+
+export default App
